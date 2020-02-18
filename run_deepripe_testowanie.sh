a7fcdd1a7ca86349a9b9fb198a5fc32b045d5f58
ilona-grabowicz <55700527+ilona-grabowicz@users.noreply.github.com>
2020-02-18 13:37:14 +0100

    Added files via upload

---
+++ run_deepripe_testowanie.sh
@@ -0,0 +1,3 @@
+python3 /home/ilona/Desktop/DeepRiPe-master/Scripts/models.py -nt 2 -ll 150 -rl 250 -ne 20 -bs 128 -dp /home/ilona/Desktop/DeepRiPe-master/test3.h5 -op /home/ilona/Desktop/DeepRiPe-master/Output_symfonia -mp /home/ilona/Desktop/DeepRiPe-master/a.h5 -pp /home/ilona/Desktop/DeepRiPe-master/Output/p.h5 -fun create_model1 -name my_model -nfl 90 -nfr 100 -fll 5 -flr 7 -p True -t True
+
+# dla high powinno byc -nt 11; dla med -nt 21 a dla low -nt 27
